3694fa801687bbc44a07d66a0c73073613303a09
Nicholas Alipaz <nicholas.alipaz@heremedia.com>
2017-10-02 17:45:43 -0700

    try pre_ci_boot again

--- inadyn/container/entrypoint.sh
+++ inadyn/container/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-if [ ${SHIPPABLE} ]; then
-  mkdir /data/
-  touch /data/options.json
-  echo ${TEST_OPTIONS} > /data/options.json
-fi
-
 mustache-cli /data/options.json /templates/inadyn.mustache > /usr/local/etc/inadyn.conf
 cat /usr/local/etc/inadyn.conf
 
